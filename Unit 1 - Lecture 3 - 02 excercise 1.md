812fa8efa895c1fd8307d83ba038ec5de2cda41a
Mingyang Zhao <pm.z@outlook.com>
2017-10-21 14:01:41 +0800

    morning

---
+++ Unit 1 - Lecture 3 - 02 excercise 1.md
@@ -0,0 +1,15 @@
+# Students in a line
+
+Second graders are lining up to go to their next class, but must be ordered alphabetically before they can leave. The teacher only swaps the positions of two students that are next to each other in line.
+
+If we want to represent this situation as a graph, which variables should be represented as edges and vertices?
+
+ - A) Vertices represent permutations of the students in line. Edges connect two permutations if one can be made into the other by swapping two adjacent students.correct
+ - B) Vertices represent students. Edges connect two students if they are next to each other in line.
+ - C) Vertices represent permutations of the students, and each edge represents an individual student. An edge connects two vertices if that student is involved in swap between the two permutations.
+
+Answer: A
+
+ ## Explaination
+ - A) is correct. Travelling from one vertex to another through an edge represents a legal move.
+ - B) only offers information relevant to the current state of the line. There is no information regarding how the line can change.
---
+++ Unit 1 - Lecture 3 - 04 graph.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 15:04:56 2016
+
+@author: amblizer
+"""
+# Node is basically the same as "object", but is useful for later revise
+class Node(object):
+    def __init__(self, name):
+        """Assumes name is a string"""
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge(object):
+    def __init__(self, src, dest):
+        """Assumes src and dest are nodes"""
+        self.src = src
+        self.dest = dest
+    def getSource(self):
+        return self.src
+    def getDestination(self):
+        return self.dest
+    def __str__(self):
+        return self.src.getName() + '->' + self.dest.getName()
+
+class Digraph(object):
+    """edges is a dict mapping each node to a list of
+    its children"""
+    def __init__(self):
+        self.edges = {}
+    def addNode(self, node):
+        if node in self.edges:
+            raise ValueError('Duplicate node')
+        else:
+            self.edges[node] = []
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not (src in self.edges and dest in self.edges):
+            raise ValueError('Node not in graph')
+        # append dest to the value of key src
+        self.edges[src].append(dest)
+    def childrenOf(self, node):
+        return self.edges[node]
+    def hasNode(self, node):
+        return node in self.edges
+    def getNode(self, name):
+        for n in self.edges:
+            if n.getName() == name:
+                return n
+        raise NameError(name)
+    def __str__(self):
+        result = ''
+        for src in self.edges:
+            for dest in self.edges[src]:
+                result = result + src.getName() + '->'\
+                + dest.getName() + '\n'
+            # ommit final empty line
+        return result[:-1]
+
+ # Graph iherit all tribute of Digraph, and make substitute
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+
+# use graphtype chosing graph or diagraph
+def buildCityGraph(graphType):
+    g = graphType()
+    for name in ('Boston', 'Providence', 'New York', 'Chicago',
+                 'Denver', 'Phoenix', 'Los Angeles'): #Create 7 nodes
+        g.addNode(Node(name))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('Providence')))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Boston')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
+    return g
+
+print(buildCityGraph(Graph))
+print()
+print(buildCityGraph(Digraph))
