fac156186f153c52a9742a1ae48cbfca8f08a0dc
Mingyang Zhao <pm.z@outlook.com>
2017-10-29 11:54:46 +0800

    lec4

--- Unit 2 - Lecture 4 - 02 visualization of data.py
+++ Unit 2 - Lecture 4 - 02 visualization of data.py
@@ -68,10 +68,76 @@ for i in range(30):
 # plt.title('Linear vs. Quadratic')
 
 
-plt.figure('lin quad')
-plt.clf()
-plt.plot(mySample, myLinear, 'b--', label = "Linear", linewidth = 5.0)
-plt.plot(mySample, myQuadratic, 'ro', label = "quadratic", linewidth = 2.0)
-plt.yscale('log')   # using log scale in y axis
-plt.legend(loc = 'upper left')
-plt.title('Linear vs. Quadratic')
+# plt.figure('lin quad')
+# plt.clf()
+# plt.plot(mySample, myLinear, 'b--', label = "Linear", linewidth = 5.0)
+# plt.plot(mySample, myQuadratic, 'ro', label = "quadratic", linewidth = 2.0)
+# plt.yscale('log')   # using log scale in y axis
+# plt.legend(loc = 'upper left')
+# plt.title('Linear vs. Quadratic')
+
+# monthly is monthly deposits
+def retire(monthly, rate, terms):
+    savings = [0]
+    base = [0]
+    mRate = rate / 12
+    for i in range(terms):
+        base += [i]
+        savings += [savings[-1] * (1 + mRate) + monthly]
+    return base, savings
+
+def displayRetireWMonthlies(monthlies, rate, terms):
+    plt.figure('retireMonth')
+    plt.clf()
+    for monthly in monthlies:
+        xvals, yvals= retire(monthly, rate, terms)
+        plt.plot(xvals, yvals, label = 'retire:' + str(monthly))
+        plt.legend(loc = 'upper left')
+    plt.show()
+
+# monthlies = [500, 600, 700 ,800, 900, 1000, 1100]
+# rate = 0.05
+# terms = 40*12
+# displayRetireWMonthlies(monthlies, rate, terms)
+
+# #%%
+# import matplotlib.pyplot as plt
+# import matplotlib as mpl
+# import numpy as np
+
+# x = np.linspace(0, 20, 100)
+# plt.plot(x, np.sin(x))
+# plt.show() 
+
+#%%
+# def displayRetreWRates(month, rates, terms):
+#     plt.figure('retireRate')
+#     plt.clf()
+#     for rate in rates:
+#         xvals, yvals = retire(month, rate, terms)
+#         plt.plot(xvals, yvals, label = 'retire:' + str(month) + ':' + str(int(rate*100)))
+#         plt.legend(loc = 'upper left')
+#     plt.show()
+# displayRetreWRates(800, [0.03, 0.05, 0.07], 40*12)
+
+
+def displayRetireWMonthsAndRates(monthlies, rates, terms):
+    plt.figure('retireBoth')
+    plt.clf()
+    plt.xlim(30*12, 40*12)  # just display diferences in last 10 years
+    monthLabels = ['r', 'b', 'g', 'k']
+    rateLabels = ['-', 'o', '--', '^']
+    for i in range(len(monthlies)):
+        monthly = monthlies[i]
+        monthLabel = monthLabels[i%len(monthLabels)]
+        for j in range(len(rates)):
+            rate = rates[j]
+            rateLabels = rateLabels[j%len(rateLabels)]
+            xvals, yvals = retire(monthly, rate, terms)
+            plt.plot(xvals, yvals, monthLabel + rateLabels, label = 'retire:' + str(monthly) + ':' \
+                                            + str(int(rate*100)))
+            plt.legend(loc = 'upper left')
+    plt.show()
+
+displayRetireWMonthsAndRates([500, 700, 900, 1100],
+                              [0.03, 0.05, 0.07], 40*12)
